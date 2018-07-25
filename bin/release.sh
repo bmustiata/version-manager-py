994218f1a8e681e18c77d854789cd121ff3ba773
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-07-25 18:15:41 +0200

    Updated release script.

--- bin/release.sh
+++ bin/release.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 cd $(readlink -f "$(dirname "$0")/..")
+
+VERSION=$(version-manager --display version)
+
+git tag -m "release $VERSION" $VERSION
+git push github --tags
+
 python setup.py sdist upload -r pypitest
 python setup.py sdist upload -r pypimain
 

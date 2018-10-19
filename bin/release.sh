78f047669b7490ae4a2ccb0d56f7d1481fd3fc48
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-19 06:11:25 +0200

    Push only after release.

--- bin/release.sh
+++ bin/release.sh
@@ -6,10 +6,10 @@ cd $(readlink -f "$(dirname "$0")/..")
 
 VERSION=$(version-manager --display version)
 
-git tag -f -m "release $VERSION" $VERSION
-git push -f github --all
-git push -f github --tags
-
 python setup.py sdist upload -r pypitest
 python setup.py sdist upload -r pypimain
+
+#git tag -f -m "release $VERSION" $VERSION
+git push -f github --all
+git push -f github --tags
 
